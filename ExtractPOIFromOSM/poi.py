59a6b38f3eb52a5d3bc30763a213894bda7b7043
Kasturi Raghavan <k@pactubuntu.(none)>
2012-08-16 20:57:53 -0700

    Script extracts amenities into json.

--- ExtractPOIFromOSM/poi.py
+++ ExtractPOIFromOSM/poi.py
@@ -1,5 +1,6 @@
 from imposm.parser import OSMParser as P
 from itertools import chain, groupby
+import json
 
 nested_nodes = []
 p = P(concurrency=4, nodes_callback=lambda x: nested_nodes.append(x))
@@ -19,7 +20,10 @@ types_of_amenities = sorted(types_of_amenities, key=lambda (k, count): count, re
 
 grouped_amenities = groupby(sorted_amenities, lambda (tags, coords): tags['amenity'])
 amenity_locations = [(k, [coord for (tags, coord) in L]) for (k, L) in grouped_amenities]
-amentiy_locations = [(k, L) for (k, L) in amenity_locations if len(L) > 20]
-
-
+amenity_locations = [(k, L) for (k, L) in amenity_locations if len(L) > 20]
+amenity_locations = dict(amenity_locations)
+serialized_data = json.dumps(amenity_locations)
+f = open('los-angeles.amenities.json', 'w');
+f.write(serialized_data);
+f.close()
 
